cf5690f7c556d1c82468017b3fda79b4f772a796
dave1780 <120386755+dave1780@users.noreply.github.com>
2023-02-20 18:10:50 +0545

    3 files modified

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+const { strictEqual } = require("assert");
 const mongoose = require("mongoose")
 const uri = "mongodb+srv://Dave150:Dave90@cluster1.xu4paq5.mongodb.net/?retrywrites=true&w=majority";
+mongoose.set('strictQueryt',true)
 mongoose.connect(uri,{useNewUrlParser:true,
 useCreateIndex:true})
 const connection = mongoose.connection;
