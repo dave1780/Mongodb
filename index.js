72ecabbf2364322efab5bdb2e40aee21cd0e76d5
dave1780 <120386755+dave1780@users.noreply.github.com>
2023-02-20 08:12:03 +0545

    2 files modified

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-
+const mongoose = require("mongoose")
+const uri = "mongodb+srv://Dave150:Dave90@cluster1.xu4paq5.mongodb.net/?retrywrites=true&w=majority";
+mongoose.connect(uri,{useNewUrlParser:true,})
