bc5d1c9c812f621d37ad3ccd6a3d849629a448cd
dave1780 <120386755+dave1780@users.noreply.github.com>
2023-02-20 18:14:06 +0545

    1 file modified

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const { strictEqual } = require("assert");
 const mongoose = require("mongoose")
 const uri = "mongodb+srv://Dave150:Dave90@cluster1.xu4paq5.mongodb.net/?retrywrites=true&w=majority";
-mongoose.set('strictQuery',true)
+mongoose.set('strictQuery',true);
+
 mongoose.connect(uri,{useNewUrlParser:true,
-useCreateIndex:true})
+useCreateIndex:true});
 const connection = mongoose.connection;
 connection.once('open',(strem)=>{
   console.log("database succesfully  connected ")
