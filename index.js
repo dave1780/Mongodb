6a362601cc639c1299d2d5addbb5b7df1d88e2b9
dave1780 <120386755+dave1780@users.noreply.github.com>
2023-02-21 13:21:04 +0545

    1 file modified

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
+const mongoose = require('mongoose');
 
-const mongoose = require("mongoose")
-require("dotenv").config("./env")
-mongoose.set("strictQuery",false)
-const uri =process.env.uri;
-mongoose.connect(uri,{useNewUrlParser:true,
-useUnifiedTopology:true}).then(()=>{
-  console.log("database is connected")
-}).catch(()=>{
-  console.log("error occured ")
-})
+require('dotenv').config('./env');
+
+mongoose.set('strictQuery', false);
+
+const uri = process.env.uri;
+
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('database is connected');
+  })
+  .catch(() => {
+    console.log('error occured ');
+  });
